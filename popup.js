3fcda8ef16d437bcd4cd6ef7d34f5a2cb311d3c7
elenamnt98 <moundoelena@gmail.com>
2026-06-12 23:51:44 +0300

    changes

--- popup.js
+++ popup.js
@@ -7,7 +7,7 @@ popup.innerHTML = `
 
       <h2>A dreamy ritual is coming</h2>
 
-      <form id="waitlist-form">
+      <form id="waitlist-form" method="POST"/>
         <input 
           type="email" 
           placeholder="Email"
@@ -21,4 +21,30 @@ popup.innerHTML = `
   </div>
 `;
 
-setTimeout(() => {document.body.appendChild(popup);}, 5000);
+setTimeout(() => {document.body.appendChild(popup);}, 3000);
+
+popup.querySelector(".close-btn").addEventListener("click", () => {
+  popup.remove();
+});
+
+popup.querySelector("#waitlist-form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const email = e.target.querySelector("input[type='email']").value;
+   try {
+      const response = await fetch("/.netlify/functions/waitlist", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email }),
+      });
+
+      if (response.ok) {
+        window.location.href = "/gradient.html";
+      }
+
+    } catch (error) {
+      console.error(error);
+    }
+});
+
